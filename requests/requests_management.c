245b115f2c44bb7303c6dffa209066c3f49bb7de
PopTurtle <kilian.map@gmail.com>
2024-12-16 17:23:16 +0100

    Ajout d'un gestionnaire de requetes vide

---
+++ requests/requests_management.c
@@ -0,0 +1,5 @@
+#include "requests_management.h"
+
+void manage_request([[maybe_unused]] request) {
+
+}
---
+++ requests/requests_management.h
@@ -0,0 +1,8 @@
+#ifndef REQUESTS_MANAGEMENT__H
+#define REQUESTS_MANAGEMENT__H
+
+#include "communication.h"
+
+extern void manage_request(request r);
+
+#endif
