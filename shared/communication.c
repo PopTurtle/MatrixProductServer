2a8b3ca3f7f6eb1ab5c6cbc3150e7aafe44f2d5c
PopTurtle <kilian.map@gmail.com>
2024-12-16 19:00:05 +0100

    Ajout de la manipulation de requetes

--- shared/communication.c
+++ shared/communication.c
@@ -2,8 +2,7 @@
 
 #define REQUEST_SIZE (4 * sizeof(int) + sizeof(pid_t))
 
-/*
-struct request {
+struct request { // ???????????????????????????????????????????????????????
 	// PID du client
 	pid_t pid;
 
@@ -14,27 +13,75 @@ struct request {
 
 	// Borne superieure des elements
 	int sup;
-};*/
+};
+
+request *request_empty() {
+    request *r = malloc(sizeof *r);
+    if (r == NULL) {
+        return NULL;
+    }
+    return r;
+}
+
+request *request_from(pid_t pid, int m, int n, int p, int sup) {
+    request *r = malloc(sizeof *r);
+    if (r == NULL) {
+        return NULL;
+    }
+    r->pid = pid;
+    r->m = m;
+    r->n = n;
+    r->p = p;
+    r->sup = sup;
+    return r;
+}
+
+void request_dispose(request **r) {
+    free(*r);
+    *r = NULL;
+}
+
+pid_t request_pid(const request *r) {
+    return r->pid;
+}
+
+void request_mat_size_a(const request *r, int s[2]) {
+    s[0] = r->m;
+    s[1] = r->n;
+}
+
+void request_mat_size_b(const request *r, int s[2]) {
+    s[0] = r->n;
+    s[1] = r->p;
+}
+
+int request_sup(const request *r) {
+    return r->sup;
+}
+
+void response_pipe_name(char *buff, pid_t pid) {
+    sprintf(buff, RESPONSE_PIPE_PREFIX "%ld", (long) pid);
+}
 
 static char *__mk_request(const request *r) {
     char *rs = malloc(REQUEST_SIZE);
     if (r == NULL) {
         return NULL;
     }
-    *rs = r->m;
-    *(rs + sizeof(int)) = r->n;
-    *(rs + sizeof(int) * 2) = r->p;
-    *(rs + sizeof(int) * 3) = r->sup;
-    *(rs + sizeof(int) * 4) = r->pid;
+    * (int *) rs = r->m;
+    * (int *) (rs + sizeof(int)) = r->n;
+    * (int *) (rs + sizeof(int) * 2) = r->p;
+    * (int *) (rs + sizeof(int) * 3) = r->sup;
+    * (pid_t *) (rs + sizeof(int) * 4) = r->pid;
     return rs;
 }
 
-static void __rd_request(request *r_out, const char *r_str) {
-    r_out->m = *(r_str);
-    r_out->n = *(r_str + sizeof(int));
-    r_out->p = *(r_str + sizeof(int) * 2);
-    r_out->sup = *(r_str + sizeof(int) * 3);
-    r_out->pid = *(r_str + sizeof(int) * 4);
+static void __rd_request(request *r_out, const void *r_str) {
+    r_out->m = *((int *) r_str);
+    r_out->n = *((int *) r_str + 1);
+    r_out->p = *((int *) r_str + 2);
+    r_out->sup = *((int *) r_str + 3);
+    r_out->pid = *((pid_t *) ((int *) r_str + 4));
 }
 
 int send_request(int r_fd, const request *r) {
@@ -70,7 +117,7 @@ int listen_request(int r_fd, request *r_out) {
 	}
 
     // Extrait les données
-    __rd_request(r_out, r_str);
+    __rd_request(r_out, (const void *) r_str);
     free(r_str);
     return 0;
 }
--- shared/communication.h
+++ shared/communication.h
@@ -4,6 +4,8 @@
 #define SERVER_PIPE_NAME "fifo_server_mp_89716355745231"
 #define RESPONSE_PIPE_PREFIX "response_mp_376517187_"
 
+#define RESPONSE_PIPE_BUFF_SIZE 64
+
 #include <fcntl.h>
 #include <sys/types.h>
 #include <stdio.h>
@@ -11,23 +13,30 @@
 #include <stdlib.h>
 #include <sys/types.h>
 
-// Structure utilisée pour les requetes
+
+// Structure utilisee pour les requetes
 typedef struct request request;
 
+// Renvoie une requete non initialisee, NULL en cas d'erreur d'allocation
+extern request *request_empty();
 
+// Renvoie une requete initialisee, NULL en cas d'erreur d'allocation
+// Les matrices sont de tailles (m * n) et (n * p)
+extern request *request_from(pid_t pid, int m, int n, int p, int sup);
 
-struct request { // ???????????????????????????????????????????????????????
-	// PID du client
-	pid_t pid;
+// Desalloue les ressources associees a la requete
+extern void request_dispose(request **r);
 
-	// Taille des matrices (m * n et n * p)
-	int m;
-	int n;
-	int p;
+// Recuperation des donnees des requetes
+extern pid_t request_pid(const request *r);
+extern void request_mat_size_a(const request *r, int s[2]);
+extern void request_mat_size_b(const request *r, int s[2]);
+extern int request_sup(const request *r);
 
-	// Borne superieure des elements
-	int sup;
-};
+// Fabrique le nom d'un tube de reponse a partir d'un pid
+// buff doit avoir une longueure minimum de RESPONSE_PIPE_BUFF_SIZE
+extern void response_pipe_name(char *buff, pid_t pid);
+
 
 // Envoie une requete sur le tube de descripteur r_fd,
 // renvoie -1 en cas d'erreur
