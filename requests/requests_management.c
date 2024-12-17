273334efbcb29ca8d1aad9b75ea6b7e4d3f27765
PopTurtle <kilian.map@gmail.com>
2024-12-17 17:07:43 +0100

    Les requetes revnoie maintenant une reponse (2 matrices + le produit qui n'est pas fait)

--- requests/requests_management.c
+++ requests/requests_management.c
@@ -13,7 +13,7 @@
     ((int *) (shm + sizeof(int) + MAT_SIZE(dim_a[0], dim_a[1])))
 
 #define SHM_MAT_C \
-    ((int *) (shm + sizeof(int) + MAT_SIZE(dim_a[0], dim_a[1]) + MAT_SIZE(dim_b[0], dim_b[1])));
+    ((int *) (shm + sizeof(int) + MAT_SIZE(dim_a[0], dim_a[1]) + MAT_SIZE(dim_b[0], dim_b[1])))
 
 #define STEP_START 0
 #define STEP_MAT_GENERATED 1
@@ -61,7 +61,12 @@ void worker_a(const request *r) {
     // Attend que le calcul matriciel soit terminé
     while (*meeting != STEP_PRODUCT_ENDED) { ; }
 
-
+    // Transmet le resultat
+    if (send_response(r, (const int *) mat_a, SHM_MAT_B, SHM_MAT_C) == -1) {
+        fprintf(stderr, "Impossible d'envoyer la reponse\n");
+        exit(EXIT_FAILURE);
+    }
+    exit(EXIT_SUCCESS);
 }
 
 void worker_b(const request *r, char *shm) {
@@ -71,7 +76,7 @@ void worker_b(const request *r, char *shm) {
     request_mat_size_b(r, dim_b);
     
     volatile int *mat_b = SHM_MAT_B;
-    random_matrix((int *) mat_b, dim_b[0], dim_a[1], request_sup(r));
+    random_matrix((int *) mat_b, dim_b[0], dim_b[1], request_sup(r));
 
     // Attend que la matrice A soit generee avant de continuer
     volatile int *meeting = SHM_MEETING;
@@ -82,8 +87,6 @@ void worker_b(const request *r, char *shm) {
     volatile int *mat_c = SHM_MAT_C;
     multithreaded_matrix_product((int *) mat_c, (int *) mat_a, (int *) mat_b);
     *meeting = STEP_PRODUCT_ENDED;
-
-    print_matrix("MAT A:", (int *) mat_b, dim_b[0], dim_b[1]);
 
     exit(EXIT_SUCCESS);
 }
